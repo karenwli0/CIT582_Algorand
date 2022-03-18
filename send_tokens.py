53fb4fafcea10e6156bec4ca16509bbcbf59af4a
Karen Li <77417225+karenwli0@users.noreply.github.com>
2022-03-18 00:36:11 -0700

    Add files via upload

--- send_tokens.py
+++ send_tokens.py
@@ -33,11 +33,13 @@ def send_tokens( receiver_pk, tx_amount ):
                                 send_amount, flat_fee=True)
     signed_tx = tx.sign(sk)
 
-    tx_confirm = acl.send_transaction(signed_tx)
-    print(signed_tx.transaction.get_txid())
-    wait_for_confirmation(acl, txid=signed_tx.transaction.get_txid())
-    sender_pk = signed_tx.transaction.sender
-
+    try:
+        tx_confirm = acl.send_transaction(signed_tx)
+        print(signed_tx.transaction.get_txid())
+        wait_for_confirmation(acl, txid=signed_tx.transaction.get_txid())
+        # sender_pk = signed_tx.transaction.sender
+    except Exception as e:
+        print(e)
     return sender_pk, txid
 
 # Function from Algorand Inc.
