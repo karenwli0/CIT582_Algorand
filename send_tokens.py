ab22ead0f9a357a5500f47d18622f716380dd6e5
Karen Li <77417225+karenwli0@users.noreply.github.com>
2022-03-18 00:27:26 -0700

    Add files via upload

--- send_tokens.py
+++ send_tokens.py
@@ -35,8 +35,7 @@ def send_tokens( receiver_pk, tx_amount ):
 
     tx_confirm = acl.send_transaction(signed_tx)
     txid = signed_tx.transaction.get_txid()
-    txinfo = wait_for_confirmation(acl, txid)
-
+    # txinfo = wait_for_confirmation(acl, txid)
     sender_pk = signed_tx.transaction.sender
 
     return sender_pk, txid
